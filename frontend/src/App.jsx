3790ffd66d7de2d9a329b770a063e009330c6a69
Shyam-rachuti <shyamrachuti@gmail.com>
2026-08-02 20:10:45 +0530

    Added task creation functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,49 @@
+import { useState } from "react"
+
 function App() {
+
+  const [tasks, setTasks] = useState([
+    "Complete DSA practice",
+    "Submit assignment",
+    "Work on project"
+  ])
+
+  const [newTask, setNewTask] = useState("")
+
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask])
+      setNewTask("")
+    }
+  }
+
   return (
     <div>
       <h1>Smart Student Task Manager</h1>
+
       <h2>Welcome, Shyam 👋</h2>
 
       <h3>Today's Tasks</h3>
 
       <ul>
-        <li>Complete DSA practice</li>
-        <li>Submit assignment</li>
-        <li>Work on project</li>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+          </li>
+        ))}
       </ul>
 
-      <button>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Enter a task"
+      />
+
+      <button onClick={addTask}>
         Add Task
       </button>
+
     </div>
   )
 }
